3affea49a6c1e1972618ff36b820b33bb0494c90
wanglihui <1162176839@qq.com>
2024-04-24 11:45:32 +0800

    文件上传添加下载

--- demo-ratelimit-redis/src/main/java/com/xkcoding/ratelimit/redis/config/RedisConfig.java
+++ demo-ratelimit-redis/src/main/java/com/xkcoding/ratelimit/redis/config/RedisConfig.java
@@ -3,6 +3,7 @@ package com.xkcoding.ratelimit.redis.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.data.redis.core.script.RedisScript;
 import org.springframework.scripting.support.ResourceScriptSource;
---
+++ demo-upload-download/src/main/java/com/xkcoding/upload/controller/DownloadController.java
@@ -0,0 +1,45 @@
+package com.xkcoding.upload.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @author WLH
+ * @description TODO
+ * @date 2024-04-24 11:39
+ */
+@Slf4j
+public class DownloadController {
+
+  @GetMapping("/downloadFile/{fileName:.+}")
+  public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+    // Load file as Resource
+    Resource resource = new ClassPathResource(fileName);
+
+    // Try to determine file's content type
+    String contentType = null;
+    try {
+      contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+    } catch (IOException ex) {
+      log.info("Could not determine file type.");
+    }
+
+    // Fallback to the default content type if type could not be determined
+    if(contentType == null) {
+      contentType = "application/octet-stream";
+    }
+    return ResponseEntity.ok()
+      .contentType(MediaType.parseMediaType(contentType))
+      .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+      .body(resource);
+  }
+}
